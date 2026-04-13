97ae328fdbf92be169e4a0c7f8cf0ff285d1bdff
Sára Goldscheiderová <sara.goldscheiderova@gmail.com>
2026-04-13 19:08:29 +0200

    feat: add theme provider to app

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { TooltipProvider } from '@/components/ui/tooltip'
+import { ThemeProvider } from '@/components/ThemeProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TooltipProvider>
-      <App />
-    </TooltipProvider>
-  </StrictMode>,
+    <ThemeProvider defaultTheme="light">
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
+    </ThemeProvider>
+  </StrictMode>
 )
