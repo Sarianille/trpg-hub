3fdc3c389318fa3bf7548b9f762ff6aa1be8900b
Sára Goldscheiderová <sara.goldscheiderova@gmail.com>
2026-06-19 01:47:25 +0200

    feat: detect browser lang and save lang changes to localstorage

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,20 +1,29 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import en from '@/locales/en.json'
 import cs from '@/locales/cs.json'
 import sk from '@/locales/sk.json'
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: en },
-    cs: { translation: cs },
-    sk: { translation: sk },
-  },
-  fallbackLng: 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-  pluralSeparator: '_',
-})
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      cs: { translation: cs },
+      sk: { translation: sk },
+    },
+    supportedLngs: ['en', 'cs', 'sk'],
+    fallbackLng: 'en',
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+    pluralSeparator: '_',
+  })
 
 export default i18n
