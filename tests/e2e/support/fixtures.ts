0324e1a0cc185dc2cfda7697c3ab60f4035a245c
Sára Goldscheiderová <sara.goldscheiderova@gmail.com>
2026-06-20 02:00:09 +0200

    fix: proper parsing of games

--- tests/e2e/support/fixtures.ts
+++ tests/e2e/support/fixtures.ts
@@ -2,11 +2,11 @@ import { test as base } from '@playwright/test'
 import { mockGames } from './mocks'
 import type { Game } from '../../../src/types'
 
-export const test = base.extend<{ seedGames: Game[] }>({
-  seedGames: [[], { option: true }],
-  page: async ({ page, seedGames }, use) => {
+export const test = base.extend<{ seed: { games: Game[] } }>({
+  seed: [ { games: [] }, { option: true }],
+  page: async ({ page, seed }, use) => {
     await page.addInitScript(() => localStorage.setItem('i18nextLng', 'en'))
-    await mockGames(page, seedGames)
+    await mockGames(page, seed.games)
     await use(page)
   }
 })
